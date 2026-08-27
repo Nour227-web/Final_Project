67668c0b6a9f105bb7fec2a3201607f756ed51ce
hhanaali1310-lab <hhanaali1310@gmail.com>
2026-08-27 04:01:47 +0300

    Resolve merge conflict

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,6 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
 use App\Http\Controllers\ProductController;
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\DashboardController;
@@ -9,13 +8,39 @@ use App\Http\Controllers\AdminController;
 use App\Http\Controllers\CustomerController;
 use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\CartController;
+use App\Http\Controllers\OrderController;
+use App\Http\Controllers\ReviewController;
+use App\Http\Controllers\CategoryController;
+use App\Http\Controllers\BrandController;
 
 
-Route::get('/', function () {
-    return view('welcome');
-});
+/*
+|--------------------------------------------------------------------------
+| Home
+|--------------------------------------------------------------------------
+*/
+
+Route::get('/', [HomeController::class, 'index'])->name('home');
+
+Route::get('/home', [HomeController::class, 'index'])->name('home');
 
 
+/*
+|--------------------------------------------------------------------------
+| Authentication
+|--------------------------------------------------------------------------
+*/
+
+Auth::routes();
+
+
+/*
+|--------------------------------------------------------------------------
+| Products
+|--------------------------------------------------------------------------
+*/
+
+// Admin: Create, Store, Edit, Update, Delete
 Route::middleware(['auth', 'role:admin'])->group(function () {
 
     Route::resource('products', ProductController::class)
@@ -28,6 +53,47 @@ Route::middleware(['auth'])->group(function () {
 
     Route::resource('products', ProductController::class)
         ->only(['index', 'show']);
+
+});
+
+
+/*
+|--------------------------------------------------------------------------
+| Product Search / Filter / Sort
+|--------------------------------------------------------------------------
+*/
+
+Route::get('/search', [ProductController::class, 'search'])
+    ->name('products.search');
+
+Route::get('/products/filter', [ProductController::class, 'filter'])
+    ->name('products.filter');
+
+Route::get('/products/sort', [ProductController::class, 'sort'])
+    ->name('products.sort');
+
+
+/*
+|--------------------------------------------------------------------------
+| Categories & Brands
+|--------------------------------------------------------------------------
+*/
+
+Route::middleware(['auth'])->group(function () {
+
+    Route::resource('categories', CategoryController::class);
+
+    Route::resource('brands', BrandController::class);
+
+    Route::get(
+        '/categories/{category}/products',
+        [CategoryController::class, 'products']
+    )->name('categories.products');
+
+    Route::get(
+        '/brands/{brand}/products',
+        [BrandController::class, 'products']
+    )->name('brands.products');
 
 });
 
@@ -77,16 +143,23 @@ Route::middleware(['auth', 'role:admin'])->group(function () {
 
 // Customer Dashboard
 Route::middleware(['auth', 'role:customer'])->group(function () {
-    Route::get('/customer/dashboard', [CustomerController::class, 'index'])->name('customer.dashboard');
+
+    Route::get('/customer/dashboard', [CustomerController::class, 'index'])
+        ->name('customer.dashboard');
+
 });
-Auth::routes();
 
 
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+/*
+|--------------------------------------------------------------------------
+| Orders
+|--------------------------------------------------------------------------
+*/
 
+Route::middleware(['auth', 'role:customer'])->group(function () {
 
-Route::get('/orders', [App\Http\Controllers\OrderController::class, 'index'])
-    ->name('orders.index');
+    Route::get('/orders', [OrderController::class, 'index'])
+        ->name('orders.index');
 
     Route::get('/orders/history', [OrderController::class, 'history'])
         ->name('orders.history');
@@ -131,4 +204,24 @@ Route::delete('/reviews/{review}', [ReviewController::class, 'destroy'])
 Route::middleware(['auth', 'role:customer'])->group(function () {
 
     Route::get('/cart', [CartController::class, 'index'])
-    ->name('cart.index');
+        ->name('cart.index');
+
+    Route::get('/cart/add/{id}', [CartController::class, 'add'])
+        ->name('cart.add');
+
+    Route::get('/cart/increase/{id}', [CartController::class, 'increase'])
+        ->name('cart.increase');
+
+    Route::get('/cart/decrease/{id}', [CartController::class, 'decrease'])
+        ->name('cart.decrease');
+
+    Route::get('/cart/delete/{id}', [CartController::class, 'delete'])
+        ->name('cart.delete');
+
+    Route::get('/checkout', [CartController::class, 'checkout'])
+        ->name('cart.checkout');
+
+    Route::post('/place-order', [CartController::class, 'placeOrder'])
+        ->name('cart.placeOrder');
+
+});
